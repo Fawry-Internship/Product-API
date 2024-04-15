ca8ec8b637d6dbcaf8e5d7ee09ab4475ad6e6da6
sameh-tarek <tarksame7@gmail.com>
2024-04-15 21:46:37 +0200

    some modifications

--- src/main/java/com/example/fawry/entity/Category.java
+++ src/main/java/com/example/fawry/entity/Category.java
@@ -16,8 +16,12 @@ public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, unique = true)
     private String name;
+
     private String description;
+
     @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY ,mappedBy = "category")
     @JsonIgnore
     @ToString.Exclude
--- src/main/java/com/example/fawry/entity/Product.java
+++ src/main/java/com/example/fawry/entity/Product.java
@@ -15,17 +15,26 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, unique = true)
     private String code;
+
+    @Column(nullable = false, unique = true)
     private String name;
+
     private String description;
+
+    @Column(nullable = false)
     private Double price;
+
     @ManyToOne
     @JoinColumn(name = "category_id")
     private Category category;
+
     private String brand;
     private String imageUrl;
     private LocalDate createdAt;
     private LocalDate updatedAt;
-
+    private boolean active;
 
 }
